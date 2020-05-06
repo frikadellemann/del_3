d3b12c731dc2bf1a253aa0d2335f1bd60c5d7bdd
frikadellemann <mads@mirol.dk>
2020-05-06 13:00:24 +0200

    added dictionary and encoding
    it now should make a true huffman compressed file

--- Huffman.py
+++ Huffman.py
@@ -1,10 +1,48 @@
-n = len(C)
-Q = C
-for i in range(1,n-1):
-    z = new element
-    z.left = extractMin(Q)
-    z.right = extractMin(Q)
-    z.freq = z.left.freq+z.right.freq
-    Q.insert(z)
-return extractMin(Q)
+import PQHeap
+from Element import Element
 
+##n = len(C)
+##Q = C
+##for i in range(1,n-1):
+##    z = new element
+##    z.left = extractMin(Q)
+##    z.right = extractMin(Q)
+##    z.freq = z.left.freq+z.right.freq
+##    Q.insert(z)
+##return extractMin(Q)
+
+def huffman(table):
+    pq = []
+    for i in range(len(table)):
+        PQHeap.insert(pq, Element(table[i],i))
+    
+    for i in range(len(table)-1):
+        left = PQHeap.extractMin(pq)
+        right = PQHeap.extractMin(pq)
+        e = Element(left.key + right.key, None)
+        e.left = left
+        e.right = right
+        PQHeap.insert(pq, e)
+        
+    code = ''
+    dictionary = {}
+    prnt(pq[0], code, dictionary)
+    return dictionary
+
+    
+def prnt(e, code, dictionary):
+    
+    if e.data == None:
+        code += '0'
+        prnt(e.right, code, dictionary)
+        code = code[:-1]
+        code += '1'
+        prnt(e.left, code, dictionary)
+        code = code[:-1]
+    else:
+        #print(code)
+        #print(str(e.key) + ':' + str(e.data))
+        dictionary[e.data] = code
+             
+    
+        
--- encode.py
+++ encode.py
@@ -1,5 +1,6 @@
 import bitIO
 import PQHeap
+import Huffman
 #import sys
 
 infile = open('G.jpg', 'rb')
@@ -9,37 +10,42 @@ bitstreamin = bitIO.BitReader(infile)
 bitstreamout = bitIO.BitWriter(outfile)
 
 
-#lav table og fyld med 0
+
 table = list()
 for i in range(256):
     table.append(0)
-print(len(table))
 def read(byte):
     v = 0
     for i in range(byte*8):
         v = (v << 1) | bitstreamin.readbit()
     return v    
 
-#def write(table):
+
     
 
 while True:
     x = read(1)
-    #print(x)
     if not bitstreamin.readsucces():
             break
     table[x] += 1
-    
-
 for i in range(255):
     bitstreamout.writeint32bits(table[i])
-print(table)
+bitstreamin.close()
 
+dictionary = Huffman.huffman(table)
 
-    
+infile = open('G.jpg', 'rb')
+bitstreamin = bitIO.BitReader(infile)
+
+while True:
+    x = read(1)
+    if not bitstreamin.readsucces():
+            break
+    bitstreamout.writebit(int(dictionary[x]))
+
 
 bitstreamout.writebit(0)
 bitstreamout.writebit(1)
-
 bitstreamout.close()
 bitstreamin.close()
+
