e3e51218080311c2827d5a363c3bdc61dd1aeaba
FakingSanity <60738808+FakingSanity@users.noreply.github.com>
2020-05-13 09:20:17 +0200

    Comments

--- decode.py
+++ decode.py
@@ -1,25 +1,29 @@
 import bitIO
 import PQHeap
 import Huffman
-
+# here we import the methods for reading and writing
+# the priority que and the huffman tree
 infile = open('encoded.txt', 'rb')
 outfile = open('decoded.txt', 'wb')
-
+# the files are opened in binary mode 
+# seen by 'rb' and 'wb'
 bitstreamin = bitIO.BitReader(infile)
 bitstreamout = bitIO.BitWriter(outfile)
-
+# streaming from the files
 table = list()
 totalBytes = 0
 for i in range(256):
     x = bitstreamin.readint32bits()
     table.append(x)
     totalBytes += x
-
+# Here we define that we go through all 256 inquries
 tree = Huffman.huffman(table)
 
 readBytes = 0
 position = tree[0]
-
+# it starts from the root index [0]
+# it sums up the total bytes which it uses to know when
+# it has written all out
 while readBytes < totalBytes:
     x = bitstreamin.readbit()
     if x == 0:
