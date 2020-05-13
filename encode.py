db651d35b9906ada8113216eb2c4798f59b97abc
FakingSanity <60738808+FakingSanity@users.noreply.github.com>
2020-05-13 06:02:06 +0200

    Comment changes. Still missing some

--- encode.py
+++ encode.py
@@ -1,14 +1,16 @@
 import bitIO
 import PQHeap
 import Huffman
-
+# We import the diffrent methods for reading and writing, priority que and Huffman tree 
 inpath = 'sample.txt'
 infile = open(inpath, 'rb')
 outfile = open('encoded.txt', 'wb')
-
+# The files are opened in binarymode seen by the 'rb' and 'wb'
+# We stream the files
 bitstreamin = bitIO.BitReader(infile)
 bitstreamout = bitIO.BitWriter(outfile)
-
+# A table with 256 inquires one for each of the possible instances of bytes
+# its definde as 8bits
 table = list()
 for i in range(256):
     table.append(0)
@@ -17,7 +19,9 @@ def read(byte):
     for i in range(byte*8):
         v = (v << 1) | bitstreamin.readbit()
     return v    
-
+# The while loop continues as long it has something to read
+# else it sees its job done and breaks
+# it reads from the inputfile and write it into the table
 while True:
     x = read(1)
     if not bitstreamin.readsucces():
@@ -28,7 +32,9 @@ for i in range(256):
     bitstreamout.writeint32bits(table[i])
 
 bitstreamin.close()
-
+#
+#
+#
 def prnt(e, code, dictionary):
     
     if e.data == None:
@@ -40,7 +46,9 @@ def prnt(e, code, dictionary):
         code = code[:-1]
     else:
         dictionary[e.data] = code
-
+#
+#
+#
 pq = Huffman.huffman(table)
 code = ''
 dictionary = {}
